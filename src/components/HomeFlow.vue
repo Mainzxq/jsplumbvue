506e3f393774734cac6971f52ed52bc495920a78
mainz <mainzxq@outlook.com>
2022-02-07 10:08:06 +0800

    init

---
+++ src/components/HomeFlow.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ul>
+            <div></div>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const testList = [
+    {
+        name:"测试1",
+        type:""
+    }
+]
+</script>
+
+<style lang="scss" scoped>
+
+</style>
